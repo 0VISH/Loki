811b40d19771a8737471a57491af21e5499a840c
unknown <SnowSneeze@protonmail.com>
2023-09-27 12:57:01 +0530

    omen updated api

--- build.py
+++ build.py
@@ -6,8 +6,10 @@ simd = "simd" in argv
 plat = "win"
 if "lin" in argv: plat = "lin"
 
+Omen.setBuildDir(plat, dbg)
+
 if dbg and plat=="win":
     extra = "/std:c++14 /arch:AVX /EHsc"
-    Omen.build("src/main.cc", plat, dbg, "Loki", "cl", extraSwitches=extra)
+    Omen.build("src/main.cc", "Loki", "cl", extraSwitches=extra)
 else:
-    Omen.build("src/main.cc", plat, dbg, "Loki", "clang++")
+    Omen.build("src/main.cc", "Loki", "clang++")
